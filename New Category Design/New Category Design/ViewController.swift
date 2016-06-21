4b5e846c71c508f07d6c400b575ad242690ef8fe
manoj@cerebrateinc.com <dipti@cerebrateinc.com>
2016-06-21 19:27:15 +0530

    implemented category screen using auto layout constraint

---
+++ New Category Design/New Category Design/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New Category Design
+//
+//  Created by Dipti Fulwani on 21/06/16.
+//  Copyright © 2016 Patronous Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
